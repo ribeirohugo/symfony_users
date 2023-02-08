32982faf4dcb79ebc9d4f5f984574fb19a78d0f1
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-08 14:44:57 +0000

    feat: add listUsers and singleUser controllers

---
+++ src/Controller/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[AsController]
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'listUsers', methods: ['GET'])]
+    public function listUsers(UserRepository $userRepository, SerializerInterface $serializer): Response
+    {
+        $users = $userRepository->findAll();
+
+        return new Response(
+            $serializer->serialize($users, JsonEncoder::FORMAT),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/json;charset=UTF-8']
+        );
+    }
+
+    #[Route('/users/{userId}', name: 'singleUser', methods: ['GET'])]
+    public function singleUser(int $userId, UserRepository $userRepository, SerializerInterface $serializer): Response
+    {
+        $users = $userRepository->find($userId);
+
+        if(empty($users)) {
+            return new Response("", Response::HTTP_NOT_FOUND);
+        }
+
+        return new Response(
+            $this->serializer->serialize($users, JsonEncoder::FORMAT),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/json;charset=UTF-8']
+        );
+    }
+}
