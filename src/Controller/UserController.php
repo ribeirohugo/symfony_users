fb4f63aa94b04a40054417a28b1e25a3249353a5
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-08 15:19:41 +0000

    chore: removed unused dependencies in user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,8 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsController]
