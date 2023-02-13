cdbbb24be48fe53e37ef8dde725a2b2f3dbba537
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-13 01:21:17 +0000

    docs: add user repository phpdoc

--- src/Repository/UserRepositoryInterface.php
+++ src/Repository/UserRepositoryInterface.php
@@ -8,11 +8,28 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 
 interface UserRepositoryInterface extends ServiceEntityRepositoryInterface
 {
+    /**
+     * @param int $userId
+     * @return mixed
+     */
     function find(int $userId);
 
+    /**
+     * @return mixed
+     */
     function findAll();
 
+    /**
+     * @param User $user
+     * @param bool $flush
+     * @return User
+     */
     function save(User $user, bool $flush = false): User;
 
+    /**
+     * @param User $user
+     * @param bool $flush
+     * @return void
+     */
     public function remove(User $user, bool $flush = false): void;
 }
