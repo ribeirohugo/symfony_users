fc9bb1c9cbf721453b0b2238c90e3fe2e7c674d0
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-13 01:02:30 +0000

    docs: add exceptions php documentation

--- src/Exception/InvalidRequestException.php
+++ src/Exception/InvalidRequestException.php
@@ -2,8 +2,14 @@
 
 namespace App\Exception;
 
+/**
+ * InvalidRequestException is thrown when an HTTP request doesn't fulfill business logic or syntax requirements.
+ */
 final class InvalidRequestException extends \Exception
 {
+    /**
+     * @param string $message
+     */
     public function __construct(string $message)
     {
         parent::__construct($message);
