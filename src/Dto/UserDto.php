8756f748b3321e8498e172f88aa9f5c06bd20159
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-13 21:04:40 +0000

    feat: add `UserDto`

---
+++ src/Dto/UserDto.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\DTO;
+
+/**
+ * UserDTO holds editable attributes for creating/updating a user.
+ */
+class UserDTO
+{
+    /**
+     * @var string|null
+     */
+    private ?string $name = null;
+
+    /**
+     * @var string|null
+     */
+    private ?string $email = null;
+
+    /**
+     * @var string|null
+     */
+    private ?string $password = null;
+
+    /**
+     * @var string|null
+     */
+    private ?string $phone = null;
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @param string $phone
+     */
+    public function __construct(string $name, string $email, string $password, string $phone) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return void
+     */
+    public function setPhone(string $phone): void {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return void
+     */
+    public function setPassword(string $password): void {
+        $this->password = $password;
+    }
+}
