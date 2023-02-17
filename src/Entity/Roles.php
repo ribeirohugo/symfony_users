68fd32de27dee5afddf1626917d8d3be8becd134
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-17 17:44:39 +0000

    feat: add user `Roles` class

---
+++ src/Entity/Roles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * Roles class that holds existing role constants.
+ */
+class Roles {
+    const ROLE_USER = "ROLE_USER";
+    const ROLE_ADMIN = "ROLE_ADMIN";
+}
