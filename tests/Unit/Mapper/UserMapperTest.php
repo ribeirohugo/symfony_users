63f545c8b3ca49f3a51bb9a2ca5e7467b2d0a323
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-17 18:49:32 +0000

    feat: update mapper to map roles between entity and dtos

--- tests/Unit/Mapper/UserMapperTest.php
+++ tests/Unit/Mapper/UserMapperTest.php
@@ -3,18 +3,22 @@
 namespace App\Tests\Unit\Mapper;
 
 use App\Dto\UserEditableDto;
+use App\Entity\Roles;
 use App\Entity\User;
 use App\Mapper\UserMapper;
 use App\Tests\Utils\ConstHelper;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Role\Role;
 
 class UserMapperTest extends TestCase{
     public function testEntityToDto() {
+        $expectedRoles = [Roles::ROLE_USER, Roles::ROLE_ADMIN];
         $user = new User(
             ConstHelper::USER_NAME_TEST,
             ConstHelper::USER_EMAIL_TEST,
             ConstHelper::USER_PASSWORD_TEST,
             ConstHelper::USER_PHONE_TEST,
+            $expectedRoles,
         );
         $user->setId(ConstHelper::USER_ID_TEST);
 
@@ -25,14 +29,17 @@ class UserMapperTest extends TestCase{
         $this->assertEquals($user->getPhone(), $dto->getPhone());
         $this->assertEquals($user->getCreatedAt(), $dto->getCreatedAt());
         $this->assertEquals($user->getUpdatedAt(), $dto->getUpdatedAt());
+        $this->assertEquals($expectedRoles, $dto->getRoles());
     }
 
     public function testEntityToDtoArray() {
+        $expectedRoles = [Roles::ROLE_USER, Roles::ROLE_ADMIN];
         $user = new User(
             ConstHelper::USER_NAME_TEST,
             ConstHelper::USER_EMAIL_TEST,
             ConstHelper::USER_PASSWORD_TEST,
             ConstHelper::USER_PHONE_TEST,
+            $expectedRoles,
         );
         $user->setId(ConstHelper::USER_ID_TEST);
         $users[] = $user;
@@ -45,14 +52,17 @@ class UserMapperTest extends TestCase{
         $this->assertEquals($user->getPhone(), $dto[0]->getPhone());
         $this->assertEquals($user->getCreatedAt(), $dto[0]->getCreatedAt());
         $this->assertEquals($user->getUpdatedAt(), $dto[0]->getUpdatedAt());
+        $this->assertEquals($user->getRoles(), $dto[0]->getRoles());
     }
 
     public function testUserEditableDtoToEntity() {
+        $expectedRoles = [Roles::ROLE_USER, Roles::ROLE_ADMIN];
         $editableUser = new UserEditableDto(
             ConstHelper::USER_NAME_TEST,
             ConstHelper::USER_EMAIL_TEST,
             ConstHelper::USER_PASSWORD_TEST,
             ConstHelper::USER_PHONE_TEST,
+            $expectedRoles,
         );
 
         $user = UserMapper::userEditableDtoToEntity($editableUser);
@@ -61,5 +71,6 @@ class UserMapperTest extends TestCase{
         $this->assertEquals($editableUser->getEmail(), $user->getEmail());
         $this->assertEquals($editableUser->getPassword(), $user->getPassword());
         $this->assertEquals($editableUser->getPhone(), $user->getPhone());
+        $this->assertEquals($editableUser->getRoles(), $user->getRoles());
     }
 }
