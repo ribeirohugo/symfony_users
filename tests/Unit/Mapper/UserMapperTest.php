27bea1b1030ef82b261ec5d6915f4f4a8aa6f017
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-17 18:52:08 +0000

    test: add without roles to mapper tests

--- tests/Unit/Mapper/UserMapperTest.php
+++ tests/Unit/Mapper/UserMapperTest.php
@@ -32,6 +32,25 @@ class UserMapperTest extends TestCase{
         $this->assertEquals($expectedRoles, $dto->getRoles());
     }
 
+    public function testEntityToDtoWithoutRoles() {
+        $user = new User(
+            ConstHelper::USER_NAME_TEST,
+            ConstHelper::USER_EMAIL_TEST,
+            ConstHelper::USER_PASSWORD_TEST,
+            ConstHelper::USER_PHONE_TEST,
+        );
+        $user->setId(ConstHelper::USER_ID_TEST);
+
+        $dto = UserMapper::entityToDto($user);
+
+        $this->assertEquals($user->getName(), $dto->getName());
+        $this->assertEquals($user->getEmail(), $dto->getEmail());
+        $this->assertEquals($user->getPhone(), $dto->getPhone());
+        $this->assertEquals($user->getCreatedAt(), $dto->getCreatedAt());
+        $this->assertEquals($user->getUpdatedAt(), $dto->getUpdatedAt());
+        $this->assertEquals([Roles::ROLE_USER], $dto->getRoles());
+    }
+
     public function testEntityToDtoArray() {
         $expectedRoles = [Roles::ROLE_USER, Roles::ROLE_ADMIN];
         $user = new User(
@@ -40,6 +59,27 @@ class UserMapperTest extends TestCase{
             ConstHelper::USER_PASSWORD_TEST,
             ConstHelper::USER_PHONE_TEST,
             $expectedRoles,
+        );
+        $user->setId(ConstHelper::USER_ID_TEST);
+        $users[] = $user;
+
+        $dto = UserMapper::entityToDtoArray($users);
+
+        $this->assertNotEmpty($dto);
+        $this->assertEquals($user->getName(), $dto[0]->getName());
+        $this->assertEquals($user->getEmail(), $dto[0]->getEmail());
+        $this->assertEquals($user->getPhone(), $dto[0]->getPhone());
+        $this->assertEquals($user->getCreatedAt(), $dto[0]->getCreatedAt());
+        $this->assertEquals($user->getUpdatedAt(), $dto[0]->getUpdatedAt());
+        $this->assertEquals($user->getRoles(), $dto[0]->getRoles());
+    }
+
+    public function testEntityToDtoArrayWithoutRoles() {
+        $user = new User(
+            ConstHelper::USER_NAME_TEST,
+            ConstHelper::USER_EMAIL_TEST,
+            ConstHelper::USER_PASSWORD_TEST,
+            ConstHelper::USER_PHONE_TEST,
         );
         $user->setId(ConstHelper::USER_ID_TEST);
         $users[] = $user;
@@ -72,5 +112,22 @@ class UserMapperTest extends TestCase{
         $this->assertEquals($editableUser->getPassword(), $user->getPassword());
         $this->assertEquals($editableUser->getPhone(), $user->getPhone());
         $this->assertEquals($editableUser->getRoles(), $user->getRoles());
+    }
+
+    public function testUserEditableDtoToEntityWithoutRoles() {
+        $editableUser = new UserEditableDto(
+            ConstHelper::USER_NAME_TEST,
+            ConstHelper::USER_EMAIL_TEST,
+            ConstHelper::USER_PASSWORD_TEST,
+            ConstHelper::USER_PHONE_TEST,
+        );
+
+        $user = UserMapper::userEditableDtoToEntity($editableUser);
+
+        $this->assertEquals($editableUser->getName(), $user->getName());
+        $this->assertEquals($editableUser->getEmail(), $user->getEmail());
+        $this->assertEquals($editableUser->getPassword(), $user->getPassword());
+        $this->assertEquals($editableUser->getPhone(), $user->getPhone());
+        $this->assertEquals([Roles::ROLE_USER], $user->getRoles());
     }
 }
