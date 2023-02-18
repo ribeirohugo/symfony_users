b18c66e1f30b4586e98840e359341c29f9cc32af
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-18 02:38:28 +0000

    fix: remove orm generator strategy

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,7 +28,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var Uuid
      */
     #[ORM\Column(type: UuidType::NAME, unique: true)]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
     private Uuid $externalId;
 
