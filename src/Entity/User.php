7f7eb0b51087e8e47444d50526fcf3754592febb
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-18 21:17:59 +0000

    chore: add unique to `email` user attribute

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -42,7 +42,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string
      */
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private string $email;
 
     /**
