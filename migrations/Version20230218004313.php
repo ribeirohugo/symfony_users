96aae868f2b1f83a8123444ffafab1154b4a23c2
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-18 02:15:51 +0000

    fix: update migrations `external_id` column name

--- migrations/Version20230218004313.php
+++ migrations/Version20230218004313.php
@@ -6,6 +6,7 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Symfony\Bridge\Doctrine\Types\UuidType;
 
 /**
  * Add Users external Id.
@@ -21,13 +22,13 @@ final class Version20230218004313 extends AbstractMigration
     {
         $table = $schema->getTable("users");
 
-        $table->addColumn("externalId", "string")->setNotnull(true);
+        $table->addColumn("external_id", UuidType::NAME);
     }
 
     public function down(Schema $schema): void
     {
         $table = $schema->getTable("users");
 
-        $table->dropColumn("externalId");
+        $table->dropColumn("external_id");
     }
 }
