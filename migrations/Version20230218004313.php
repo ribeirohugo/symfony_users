981be03e528d3dd73c878178774722b693446439
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-02-18 00:50:13 +0000

    feat: add user externalId database migration

--- migrations/Version20230218004313.php
+++ migrations/Version20230218004313.php
@@ -8,25 +8,26 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Adds User roles to database.
+ * Add Users external Id.
  */
-final class Version20230217175801 extends AbstractMigration
+final class Version20230218004313 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add User roles.';
+        return 'Add Users external Id.';
     }
 
     public function up(Schema $schema): void
     {
         $table = $schema->getTable("users");
 
-        $table->addColumn("roles", "json");
+        $table->addColumn("externalId", "string")->setNotnull(true);
     }
+
     public function down(Schema $schema): void
     {
         $table = $schema->getTable("users");
 
-        $table->dropColumn("roles");
+        $table->dropColumn("externalId");
     }
 }
